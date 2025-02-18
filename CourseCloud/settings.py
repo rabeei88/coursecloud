ee54d6bceac32e9e41c02e71f18baa696024131e
rabeei <mohrabeeipp4221@gmail.com>
2025-02-18 12:04:29 +0530

    'firstcommit'

--- CourseCloud/settings.py
+++ CourseCloud/settings.py
@@ -43,7 +43,17 @@ INSTALLED_APPS = [
     'instructor',
     'embed_video',
     "student",
-]
+
+    
+    # enabling social authentication
+    'django.contrib.sites',
+    
+    'allauth',
+    'allauth.account',
+
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -52,6 +62,10 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+
+    # Add the account middleware:
+    "allauth.account.middleware.AccountMiddleware",
+
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
@@ -135,3 +149,37 @@ AUTH_USER_MODEL = 'instructor.User'
 #     "SITE_HEADER": "Ajay Rajendran",
 #     "SITE_URL": "/admin/",  # Change if needed
 # }
+
+
+AUTHENTICATION_BACKENDS = [
+    
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by email
+    'allauth.account.auth_backends.AuthenticationBackend',
+    
+]
+
+
+# Provider specific settings
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        # For each OAuth based provider, either add a ``SocialApp``
+        # (``socialaccount`` app) containing the required client
+        # credentials, or list them here:
+        'APP': {
+            'client_id':config("client_id"),
+            'secret':config("secret"),
+        }
+    }
+}
+
+SITE_ID=1
+
+LOGIN_REDIRECT_URL='index'
+
+LOGIN_URL='signin'
+
+SOCIALACCOUNT_LOGIN_ON_GET=True
+
